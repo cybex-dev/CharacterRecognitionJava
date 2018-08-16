ff33a58d737fd709732c2cb83a291e0e0a930f27
KGCybeX <k4gcybex@gmail.com>
2018-08-16 02:42:41 +0200

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,15 +6,16 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Main {
-    final int numIterations = Integer.MAX_VALUE;
-    final int NumHiddenNeurons = 10;
+
+    static PrintWriter p;
+    final int numIterations = 40000;
+    final int NumHiddenNeurons = 200;
     final int NumOutputNeurons = 5;
     final int numHiddenLayerInputs = 105;
-    final double eta = 0.1; //learning rate
+    final double eta = 0.001; //learning rate
     final int bias = -1;
 
-    final double accuracyLowerBound = 0.27;
-    final double accuracyUpperBound = 0.73;
+    final double accuracy = 0.27;
 
     //    Hidden layer Weights  + 1 for bias
     double[][] hiddenLayerWeights = new double[NumHiddenNeurons][numHiddenLayerInputs + 1];
@@ -32,16 +30,20 @@ public class Main {
     double[] outputLayerFnet = new double[NumOutputNeurons];
 
     public Main() throws IOException {
+        p = new PrintWriter("output.csv");
+
         List<Character> characters = readCharactersFromFile("/char_profiles_alphabet.txt");
         List<Character> testingSet = readCharactersFromFile("/test_char_profiles_alphabet.txt");
 
         initializeEmptyWeights();
 
-        double previousTestingSSE = Double.MAX_VALUE,
-                trainingSSE = Double.MAX_VALUE,
-                currentTestingSSE = Double.MAX_VALUE;
+        double previousTestingSSE = 0.0,
+                trainingSSE = 0.0,
+                currentTestingSSE = 0.0;
 
         int iterationCount = 0;
+
+        StringBuilder printString = new StringBuilder();
 
         System.out.println("Starting training:");
         while (iterationCount < numIterations &&
@@ -51,102 +53,112 @@ public class Main {
             trainingSSE = 0.0;
             currentTestingSSE = 0.0;
 
-            System.out.println("Iteration: " + iterationCount);
+//            System.out.println("Iteration: " + iterationCount);
 //            System.out.printf("Training...");
             int trainCharCorrect = 0;
+
+            printString.append(String.valueOf(iterationCount)).append(";");
+
+
             for (int i = 0; i < characters.size(); i++) {
                 Character c = characters.get(i);
 
-                // fnet of inputs to hidden   20 * 106              20
-                computeFnet(c.getVectors(), hiddenLayerWeights, hiddenLayerFnet);
-
-                // fnet of hidden to output
-                computeFnet(hiddenLayerFnet, outputLayerWeights, outputLayerFnet);
-
-                // delta k (error signal of hidden to output)
-                double[] deltaK = computeDeltaK(outputLayerFnet, c.getBinary());
-
-                // delta J (error signal input to hidden)
-                double[] deltaJ = computeDeltaJ(hiddenLayerFnet, deltaK, outputLayerWeights);
-
-                // Adjust weights output
-                updateWeights(outputLayerWeights, deltaK, hiddenLayerFnet);
-
-                // Adjust weights hidden
-                updateWeights(hiddenLayerWeights, deltaJ, c.getVectors());
+                trainWithCharacter(c);
 
                 // Calculate SSE for pattern p
                 trainCorrect = 0;
                 for (int j = 0; j < outputLayerFnet.length; j++) {
-                    double neuronError = c.getBinary()[j] - outputLayerFnet[j];
-                    if ((c.getBinary()[j] == 0.0 && neuronError <= accuracyLowerBound)
-                            || (c.getBinary()[j] == 1.0 && neuronError >= accuracyUpperBound))
+                    double neuronError = Math.abs(c.getBinary()[j] - outputLayerFnet[j]);
+                    if (c.getBinary()[j] == 0.0 && neuronError <= accuracy) {
                         trainCorrect++;
+                    } else if (c.getBinary()[j] == 1.0 && (1-neuronError) < accuracy) {
+                        trainCorrect++;
+                    }
 
                     trainingSSE += Math.pow(neuronError, 2);
                 }
 
                 if (trainCorrect == NumOutputNeurons) {
-                    System.out.println("(Train) Correctly identified: " + c.desired);
+//                    System.out.println("(Train) Correctly identified: " + c.desired);
                     trainCharCorrect++;
                 }
-//                System.out.printf("Output:\t#1 - %f\t#2 - %f\t#3 - %f\t#4 - %f\t#5 - %f\n", outputLayerFnet[0], outputLayerFnet[1], outputLayerFnet[2], outputLayerFnet[3], outputLayerFnet[4]);
             }
 
-            if (trainCorrect == characters.size())
-                System.out.println("All training characters identified");
-
-
-            // Calculate real SSE
-            trainingSSE = (trainingSSE/(characters.size() * NumOutputNeurons));
+            printString.append(String.valueOf(trainingSSE)).append(";");
+            printString.append(String.valueOf(trainCharCorrect)).append(";");
 
             // Calculate SSE for all patterns
-            System.out.println("Train SSE: " + trainingSSE);
+//            System.out.println("Train SSE: " + trainingSSE);
 
 //            Testing
 //            System.out.println("Testing:");
-//            int testCharCorrect = 0;
-//            for (int i = 0; i < testingSet.size(); i++) {
-//                // fnet of inputs to hidden   20 * 106              20
-//                double[] arrHidden = new double[NumHiddenNeurons];
-//                computeFnet(testingSet.get(i).getVectors(), hiddenLayerWeights, arrHidden);
-//
-//                // fnet of hidden to output
-//                double[] arrOutput = new double[NumOutputNeurons];
-//                computeFnet(arrHidden, outputLayerWeights, arrOutput);
-//
-//                testCorrect = 0;
-//                for (int j = 0; j < arrOutput.length; j++) {
-//                    double neuronError = testingSet.get(i).getBinary()[j] - arrOutput[j];
-//                    currentTestingSSE = Math.pow(neuronError, 2);
-//                    if (testingSet.get(i).getBinary()[j] == 0.0 && neuronError <= accuracyLowerBound) {
-//                        testCorrect++;
-//                    } else if (testingSet.get(i).getBinary()[j] == 1.0 && neuronError >= accuracyUpperBound) {
-//                        testCorrect++;
-//                    }
-//                }
-//
-//                if (testCorrect == NumOutputNeurons) {
+            int testCharCorrect = 0;
+            for (int i = 0; i < testingSet.size(); i++) {
+                // fnet of inputs to hidden   20 * 106              20
+                double[] arrHidden = new double[NumHiddenNeurons];
+                computeFnet(testingSet.get(i).getVectors(), hiddenLayerWeights, arrHidden);
+
+                // fnet of hidden to output
+                double[] arrOutput = new double[NumOutputNeurons];
+                computeFnet(arrHidden, outputLayerWeights, arrOutput);
+
+                testCorrect = 0;
+                for (int j = 0; j < arrOutput.length; j++) {
+                    double neuronError = Math.abs(testingSet.get(i).getBinary()[j] - arrOutput[j]);
+                    if (testingSet.get(i).getBinary()[j] == 0.0 && neuronError < accuracy) {
+                        testCorrect++;
+                    } else if (testingSet.get(i).getBinary()[j] == 1.0 && (1-neuronError) < accuracy) {
+                        testCorrect++;
+                    }
+                    currentTestingSSE = Math.pow(neuronError, 2);
+                }
+
+//                System.out.println("Target:");
+//                for (int ii :testingSet.get(i).getBinary()) { System.out.printf("%1d\n", ii); }
+//                System.out.println("Output:");
+//                for (double d :arrOutput) { System.out.printf("%6.2f\n", d); }
+
+                if (testCorrect == NumOutputNeurons) {
 //                    System.out.println("(Test) Correctly identified: " + testingSet.get(i).desired);
-//                    testCharCorrect++;
-//                }
-//            }
-//
-//            if (testCharCorrect == testingSet.size())
-//                System.out.println("All testing characters identified");
-//
-//            previousTestingSSE = trainingSSE;
+                    testCharCorrect++;
+                }
+            }
+
+            if (testCharCorrect == testingSet.size()) {
+                System.out.println("All testing characters identified");
+            } else {
+                System.out.println("Identified: " + testCharCorrect);
+            }
+
+            previousTestingSSE = currentTestingSSE;
+
+            printString.append(String.valueOf(currentTestingSSE)).append(";");
+            printString.append(String.valueOf(testCharCorrect)).append(";");
+            p.write(printString.toString().concat("\n").replace(".", ","));
+            p.flush();
+            printString = new StringBuilder();
+
             iterationCount++;
         }
+    }
 
-        if (previousTestingSSE < currentTestingSSE){
-            System.out.println("Stoped due to over-fiting");
-        } else if (iterationCount == numIterations) {
-            System.out.println("Max num iterations reached");
-        } else {
-            System.out.println("Unknown stopping condition");
-        }
+    private void trainWithCharacter(Character c) {
+        // fnet of inputs to hidden   20 * 106              20
+        computeFnet(c.getVectors(), hiddenLayerWeights, hiddenLayerFnet);
 
+        // fnet of hidden to output
+        computeFnet(hiddenLayerFnet, outputLayerWeights, outputLayerFnet);
+
+        // delta k (error signal of hidden to output)
+        double[] deltaK = computeDeltaK(outputLayerFnet, c.getBinary());
+
+        // delta J will be calculated while doing updates, as a summation is to be done using the deltaK error signal calculated above
+
+        // Adjust weights output
+        updateWeightsK(outputLayerWeights, deltaK, hiddenLayerFnet);
+
+        // Adjust weights hidden
+        updateWeightsJ(hiddenLayerWeights, deltaK, c.getVectors());
     }
 
     public static void main(String[] args) {
@@ -195,45 +207,51 @@ public class Main {
         return characters;
     }
 
-    private void updateSSE() {
+    private void updateWeightsJ(double[][] weightsToUpdate, double[] delta, double[] inputVectors) {
+        // 1 offset for bias as bias is first weight
+        for (int i = 0; i < weightsToUpdate.length; i++) { // all neurons, where i is a specific neuron
+            for (int j = 1; j < weightsToUpdate[i].length; j++) { // all weights of specific neuron, j being a specific weight of the i'th neuron
+                double weightChange = 0.0;
+                for (int i1 = 0; i1 < delta.length; i1++) {
+                    weightChange -= -1.0 * delta[i1] * outputLayerWeights[i1][i] * hiddenLayerFnet[i] * (1-hiddenLayerFnet[i]) * inputVectors[j - 1];
+                }
+                weightsToUpdate[i][j] = weightsToUpdate[i][j] - (eta * weightChange);
+            }
 
+            // update bias
+            int j = 0;
+            double weightChange = 0.0;
+            for (int i1 = 0; i1 < delta.length; i1++) {
+                weightChange -= -1.0 * delta[i1] * outputLayerWeights[i1][i] * hiddenLayerFnet[i] * (1-hiddenLayerFnet[i]) * bias;
+            }
+            weightsToUpdate[i][j] = weightsToUpdate[i][j] - (eta * weightChange);
+
+        }
     }
 
-    private void updateWeights(double[][] weightsToUpdate, double[] delta, double[] inputVectors) {
+    private void updateWeightsK(double[][] weightsToUpdate, double[] delta, double[] inputVectors) {
         // 1 offset for bias as bias is first weight
-        for (int i = 0; i < delta.length; i++) {
+        for (int i = 0; i < weightsToUpdate.length; i++) {
             for (int j = 1; j < weightsToUpdate[i].length; j++) {
                 double currentWeight = weightsToUpdate[i][j];
-                double weightChange = -1.0 * eta * delta[i] * inputVectors[j - 1];
-                weightsToUpdate[i][j] = currentWeight - weightChange;
+                double weightChange = -1.0 * delta[i] * inputVectors[j - 1];
+                weightsToUpdate[i][j] = currentWeight - (eta * weightChange);
             }
 
             // update bias
             int j = 0;
             double currentWeight = weightsToUpdate[i][j];
             double weightChange = -1.0 * eta * delta[i] * bias;
-            weightsToUpdate[i][j] = currentWeight - weightChange;
+            weightsToUpdate[i][j] = currentWeight + weightChange;
 
         }
-    }
-
-    private double[] computeDeltaJ(double[] hiddenLayerFnet, double[] deltaK, double[][] outputLayerWeights) {
-        double[] deltaJ = new double[hiddenLayerFnet.length];
-        // for every output neuron getting an input from the current hidden layer neuron
-        for (int i = 0; i < hiddenLayerFnet.length; i++) {
-            for (int j = 0; j < deltaK.length; j++) {
-                double hiddenLayerOutput = hiddenLayerFnet[i];
-                deltaJ[i] = deltaK[j] * outputLayerWeights[j][i] * (1 - hiddenLayerOutput) * hiddenLayerOutput;
-            }
-        }
-        return deltaJ;
     }
 
     private double[] computeDeltaK(double[] outputLayerFnet, int[] characterBinary) {
         double[] deltaK = new double[outputLayerFnet.length];
         for (int i = 0; i < outputLayerFnet.length; i++) {
             double output = outputLayerFnet[i];
-            deltaK[i] = -1.0 * (characterBinary[i] - output) * (1 - output) * output;
+            deltaK[i] = (characterBinary[i] - output) * (1 - output) * output;
         }
         return deltaK;
     }
